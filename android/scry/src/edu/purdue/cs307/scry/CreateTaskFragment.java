18a9c1afb479eff8d077e6c3f3bba7b557b86c8c
David Tschida <dmtschida1@gmail.com>
2014-03-27 23:31:23 -0400

    Added way to return unique categories

--- android/scry/src/edu/purdue/cs307/scry/CreateTaskFragment.java
+++ android/scry/src/edu/purdue/cs307/scry/CreateTaskFragment.java
@@ -1,9 +1,12 @@
 package edu.purdue.cs307.scry;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,26 +16,37 @@ import android.widget.TextView;
 
 public class CreateTaskFragment extends Fragment {
     public static String TAG = "CreateTaskFragment";
-    
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	    Bundle savedInstanceState) {
-	final View v = inflater
-	        .inflate(R.layout.create_task, null);
+	final View v = inflater.inflate(R.layout.create_task, null);
 
 	Button b = (Button) v.findViewById(R.id.btn_create);
 	Button list = (Button) v.findViewById(R.id.btn_openlist);
-	
+	Button categories = (Button) v.findViewById(R.id.test);
+
+	categories.setOnClickListener(new OnClickListener() {
+
+	    @Override
+	    public void onClick(View v) {
+		List<String> l = ((TaskDatasourceActivity) getActivity())
+		        .getDataSource().getCategories();
+		for (String s : l) {
+		    Log.d("SQL", s);
+		}
+	    }
+
+	});
+
 	list.setOnClickListener(new OnClickListener() {
 
 	    @Override
-            public void onClick(View v) {
-	        ((MainActivity) getActivity()).pushListFragment();
-            }
-	    
+	    public void onClick(View v) {
+		((MainActivity) getActivity()).pushListFragment();
+	    }
+
 	});
-	
 
 	b.setOnClickListener(new OnClickListener() {
 
@@ -50,8 +64,9 @@ public class CreateTaskFragment extends Fragment {
 
 		    @Override
 		    protected Void doInBackground(Void... arg0) {
-			Task t = ((TaskDatasourceActivity) getActivity()).getDataSource().createComment(title, category);
-			
+			Task t = ((TaskDatasourceActivity) getActivity())
+			        .getDataSource().createComment(title, category);
+
 			return null;
 		    }
 
