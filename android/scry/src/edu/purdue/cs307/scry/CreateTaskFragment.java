33e105bf0f9557a8767fa7e2b2fc02ded45c3cee
David Tschida <dmtschida1@gmail.com>
2014-03-27 23:59:11 -0400

    Created autocompleting text view

--- android/scry/src/edu/purdue/cs307/scry/CreateTaskFragment.java
+++ android/scry/src/edu/purdue/cs307/scry/CreateTaskFragment.java
@@ -11,7 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CreateTaskFragment extends Fragment {
@@ -25,6 +28,14 @@ public class CreateTaskFragment extends Fragment {
 	Button b = (Button) v.findViewById(R.id.btn_create);
 	Button list = (Button) v.findViewById(R.id.btn_openlist);
 	Button categories = (Button) v.findViewById(R.id.test);
+
+	final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+	        android.R.layout.simple_dropdown_item_1line,
+	        ((TaskDatasourceActivity) getActivity()).getDataSource()
+	                .getCategories());
+	AutoCompleteTextView textView = (AutoCompleteTextView) v
+	        .findViewById(R.id.etxt_category);
+	textView.setAdapter(adapter);
 
 	categories.setOnClickListener(new OnClickListener() {
 
@@ -52,9 +63,9 @@ public class CreateTaskFragment extends Fragment {
 
 	    @Override
 	    public void onClick(View arg0) {
-		final TextView txttitle = (TextView) v
+		final EditText txttitle = (EditText) v
 		        .findViewById(R.id.etxt_title);
-		final TextView txtcat = (TextView) v
+		final EditText txtcat = (EditText) v
 		        .findViewById(R.id.etxt_category);
 
 		final String title = txttitle.getText().toString();
@@ -66,7 +77,7 @@ public class CreateTaskFragment extends Fragment {
 		    protected Void doInBackground(Void... arg0) {
 			Task t = ((TaskDatasourceActivity) getActivity())
 			        .getDataSource().createComment(title, category);
-
+			adapter.add(category);
 			return null;
 		    }
 
