f00ee0b11f34bd276990c2b47d593c195dbc4cc9
cbulava <cbulava@gmail.com>
2014-04-04 01:02:26 -0400

    Added what UI stuff I can, left some stuff out
    Wasn't able to test -

--- android/scry/src/edu/purdue/cs307/scry/User.java
+++ android/scry/src/edu/purdue/cs307/scry/User.java
@@ -1,5 +1,5 @@
 package edu.purdue.cs307.scry;
-<<<<<<< HEAD
+
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
@@ -28,7 +28,7 @@ import java.util.ArrayList;
  * If not using 4.3, just change version number in URL to version you are using
  * */
 
-<<<<<<< HEAD
+
 @PersistenceCapable
 public class User {
 	@Persistent
@@ -50,7 +50,7 @@ public class User {
 	
 	public User(int num, String nam, String em){
 		phone_number = num;
-=======
+
 
 public class User {
 	
@@ -67,20 +67,20 @@ public class User {
 	
 	public User(String ID, String nam, String em){
 		userID = ID;
->>>>>>> cfc2fcb31cd4fa8ee5d420a50933d11cb84b90d8
+
 		name = nam;
 		email = em;
 		tasklist = new ArrayList<Task>();
 		friendsList = new ArrayList<User>();
 	}
 	
-<<<<<<< HEAD
+
 	public int getPhone(){
 		return this.phone_number;
 =======
 	public String getUserID(){
 		return this.userID;
->>>>>>> cfc2fcb31cd4fa8ee5d420a50933d11cb84b90d8
+
 	}
 	
 	public String getName(){
@@ -120,11 +120,11 @@ public class User {
 	public String toString(){
 		String info = "";
 		info += "Name: " + this.getName();
-<<<<<<< HEAD
+
 		info += "\nPhone: " + this.getPhone();
-=======
+
 		info += "\nUser ID: " + this.getUserID();
->>>>>>> cfc2fcb31cd4fa8ee5d420a50933d11cb84b90d8
+
 		info += "\nEmail: " + this.getEmail();
 		return info;
 	}
