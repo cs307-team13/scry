542b6cd9e32fdc5bb639af93b24c7b540dae7749
David Tschida <dmtschida1@gmail.com>
2014-04-15 20:50:59 -0400

    Improved clustering and tasks show titles on click

--- android/scry/src/edu/purdue/cs307/scry/TaskMapFragment.java
+++ android/scry/src/edu/purdue/cs307/scry/TaskMapFragment.java
@@ -7,6 +7,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,16 +17,25 @@ import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 
-public class TaskMapFragment extends SupportMapFragment {
+public class TaskMapFragment extends SupportMapFragment implements
+        ClusterManager.OnClusterClickListener<Task>,
+        ClusterManager.OnClusterInfoWindowClickListener<Task>,
+        ClusterManager.OnClusterItemClickListener<Task>,
+        ClusterManager.OnClusterItemInfoWindowClickListener<Task> {
 
     private static View view;
     /**
@@ -74,13 +85,22 @@ public class TaskMapFragment extends SupportMapFragment {
 		mClusterManager = new ClusterManager<Task>(
 		        TaskMapFragment.this.getActivity(), getMap());
 		getMap().setOnCameraChangeListener(mClusterManager);
-		
+
 		mClusterManager.addItems(locTasks);
+		mClusterManager.setRenderer(new TaskRenderer());
+		getMap().setOnCameraChangeListener(mClusterManager);
+	        getMap().setOnMarkerClickListener(mClusterManager);
+	        getMap().setOnInfoWindowClickListener(mClusterManager);
+		mClusterManager.setOnClusterClickListener(TaskMapFragment.this);
+	        mClusterManager.setOnClusterInfoWindowClickListener(TaskMapFragment.this);
+	        mClusterManager.setOnClusterItemClickListener(TaskMapFragment.this);
+	        mClusterManager.setOnClusterItemInfoWindowClickListener(TaskMapFragment.this);
 		/*
 	         * getMap().addMarker(
 	         * new MarkerOptions().position(t.getLocation())
 	         * .title(t.title));
 	         */
+	        mClusterManager.cluster();
 
 	    }
 
@@ -96,4 +116,53 @@ public class TaskMapFragment extends SupportMapFragment {
 
 	return view;
     }
+
+    @Override
+    public void onClusterItemInfoWindowClick(Task item) {
+	// TODO Auto-generated method stub
+	Toast.makeText(this.getActivity(), "onClusterItemInfoWindowClick()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onClusterItemClick(Task item) {
+	// TODO Auto-generated method stub
+	Toast.makeText(this.getActivity(), "onClusterItemClick()", Toast.LENGTH_SHORT).show();
+
+	return false;
+    }
+
+    @Override
+    public void onClusterInfoWindowClick(Cluster<Task> cluster) {
+	// TODO Auto-generated method stub
+	Toast.makeText(this.getActivity(), "onClusterInfoWindowClick()", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onClusterClick(Cluster<Task> cluster) {
+	// TODO Auto-generated method stub
+	Toast.makeText(this.getActivity(), "onClusterClick()", Toast.LENGTH_SHORT).show();
+
+	return false;
+    }
+    
+    private class TaskRenderer extends DefaultClusterRenderer<Task> {
+
+        public TaskRenderer() {
+            super(TaskMapFragment.this.getActivity().getApplicationContext(), getMap(), mClusterManager);
+        }
+
+        @Override
+        protected void onBeforeClusterItemRendered(Task task, MarkerOptions markerOptions) {
+            markerOptions.title(task.title);
+        }
+
+        @Override
+        protected boolean shouldRenderAsCluster(Cluster<Task> cluster) {
+            // Always render clusters.
+            return cluster.getSize() > 4;
+        }
+    }
+    
+    
 }
