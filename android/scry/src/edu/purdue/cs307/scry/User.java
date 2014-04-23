1c8adb0c70ef401797fa23e6983e6ea947335b77
anthonynatoli <anatoli@purdue.edu>
2014-04-23 16:52:28 -0400

    Minor adjustments

--- android/scry/src/edu/purdue/cs307/scry/User.java
+++ android/scry/src/edu/purdue/cs307/scry/User.java
@@ -3,27 +3,6 @@ package edu.purdue.cs307.scry;
 
 import java.util.ArrayList;
 
-/*Persistence is what will enable the google app engine to write instances of
- * classes to the datastore, which is the database we will be using through the 
- * GAE (Google App Engine). Each class should be @PersistenceCapable and each data
- * member of the class should be @Persistent so they can all be stored in the datastore. 
- * We will use the user's email as the key to look up the user in the datastore so it 
- * will need the @PrimaryKey annotation.
- * 
- * These are important to the implementation of the server, so make sure when 
- * creating your classes that these details are included.
- * 
- * More information about the datastore, persistence, and JDO can be found here:
- * https://developers.google.com/appengine/docs/java/datastore/jdo/overview-dn2
- * 
- * Make sure you have the GAE plugin for eclipse installed so you have access to the
- * SDKs necessary.
- * 
- * For Eclipse 4.3: https://developers.google.com/eclipse/docs/install-eclipse-4.3
- * If not using 4.3, just change version number in URL to version you are using
- * */
-
-
 
 public class User {
 	
