f8e8edb0598068a32b2fbcf807d66a6afaba7c32
cbulava <cbulava@gmail.com>
2014-04-28 17:43:41 -0400

    Added Icon + Expand List Items

---
+++ android/scry/src/edu/purdue/cs307/scry/ExpandAnimation.java
@@ -0,0 +1,57 @@
+package edu.purdue.cs307.scry;
+
+import android.widget.RelativeLayout.LayoutParams;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
+
+public class ExpandAnimation extends Animation{
+	View mView;
+	LayoutParams LP;
+	LinearLayout LL;
+	boolean visibility = false;
+	int marginStart, marginEnd;
+	boolean mWasEndedAlready = false;
+
+	
+	 public ExpandAnimation(View view, int duration) {
+
+	        setDuration(duration);
+	        mView = view;
+	        LP = (LayoutParams) view.getLayoutParams();
+	        LL = (LinearLayout) view;
+	        
+	        // decide to show or hide the view
+	        visibility = (view.getVisibility() == View.VISIBLE);
+
+	        marginStart = LP.bottomMargin;
+	        marginEnd = (marginStart == 0 ? (0- view.getHeight()) : 0);
+
+	        view.setVisibility(View.VISIBLE);
+	    }
+	
+	
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+        
+        if (interpolatedTime < 1.0f) {
+            // Calculating the new bottom margin, and setting it
+            LP.bottomMargin = marginStart
+                    + (int) ((marginEnd - marginStart) * interpolatedTime);
+ 
+            // Invalidating the layout, making us seeing the changes we made
+            mView.requestLayout();
+        }  else if (!mWasEndedAlready) {
+            LP.bottomMargin = marginEnd;
+            mView.requestLayout();
+
+            if (visibility) {
+                mView.setVisibility(View.GONE);
+            }
+            mWasEndedAlready = true;
+        }
+    }
+}
+
--- android/scry/src/edu/purdue/cs307/scry/TaskArrayAdapter.java
+++ android/scry/src/edu/purdue/cs307/scry/TaskArrayAdapter.java
@@ -7,24 +7,29 @@ import android.os.Handler;
 import android.os.Looper;
 // import android.app.Fragment;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TaskArrayAdapter extends ArrayAdapter<Task> {
 
     private final Context context;
     private Fragment f;
-
+    private View PreviousToolbar = null;
+    
     public TaskArrayAdapter(Context context, int resource) {
 	super(context, resource);
 	this.context = context;
@@ -67,12 +72,73 @@ public class TaskArrayAdapter extends ArrayAdapter<Task> {
 	        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	View taskView = inflater
 	        .inflate(R.layout.task_list_item, parent, false);
+	
+	
+	taskView.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			View toolbar = v.findViewById(R.id.toolbar);
+			
+			boolean SameItemClicked = false;
+			if ((PreviousToolbar==toolbar)) {
+			PreviousToolbar = null;
+			SameItemClicked = true;
+			}
+			if (PreviousToolbar!=null){
+			ExpandAnimation tempExpandAni = new ExpandAnimation(PreviousToolbar, 500);
+			PreviousToolbar.startAnimation(tempExpandAni);
+			}
+
+			// Creating the expand animation for the item
+			ExpandAnimation expandAni = new ExpandAnimation(toolbar, 500);
+
+			// Start the animation on the toolbar
+			toolbar.startAnimation(expandAni);
+			if (SameItemClicked) {
+			PreviousToolbar = null;
+			}
+			else {
+			PreviousToolbar = toolbar;
+			}
+           
+
+            // Creating the expand animation for the item
+           // ExpandAnimation expandAni = new ExpandAnimation(toolbar, 500);
+
+            // Start the animation on the toolbar
+          //  toolbar.startAnimation(expandAni);
+		}
+	});
+	
+	
 	TextView task_name = (TextView) taskView.findViewById(R.id.task_name);
+	/*
+	task_name.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+            View toolbar = v.findViewById(R.id.toolbar);
+
+            // Creating the expand animation for the item
+            ExpandAnimation expandAni = new ExpandAnimation(toolbar, 500);
+
+            // Start the animation on the toolbar
+            toolbar.startAnimation(expandAni);
+			
+		}
+	});
+	
+	*/
+	
+	
 	TextView task_category = (TextView) taskView
 	        .findViewById(R.id.task_category);
 	CheckBox completed = (CheckBox) taskView
 	        .findViewById(R.id.task_completed);
-	if (t.isComplete()) {
+	if (t.isComplete()) { 
 	    completed.setChecked(true);
 	} else {
 	    completed.setChecked(false);
@@ -103,29 +169,18 @@ public class TaskArrayAdapter extends ArrayAdapter<Task> {
 			    }
 			}
 		    }, 1000);
-		}
+		} 	
 	    }
 	});
 	task_name.setText(t.toString());
 	task_category.setText(t.getCategory());
 
-	taskView.setOnClickListener(new OnClickListener() {
 
-	    @Override
-	    public void onClick(View v) {
-		Log.wtf("This Sucks", "in on click");
-		// ((MainActivity) frag.getActivity()).pushTaskDetailsFragment(t);
-	    }
-	});
-
-	taskView.setOnLongClickListener(new OnLongClickListener() {
-
-	    @Override
-	    public boolean onLongClick(View v) {
-		Log.wtf("Hello", "I long clicked!");
-		return true;
-	    }
-	});
+	//ADDED BY CHRIS
+//	View toolbar = convertView.findViewById(R.id.toolbar);
+	//View toolbar = ((Activity)context).getWindow().getDecorView().findViewById(R.id.toolbar);
+  //  ((LinearLayout.LayoutParams) toolbar.getLayoutParams()).bottomMargin = -50;
+  //  toolbar.setVisibility(View.GONE);
 	return taskView;
     }
 }
